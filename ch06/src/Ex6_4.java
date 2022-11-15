d9bd9b46757c4073b4243aabde79d7c9df23d0a1
김상태 <shockpower@gmail.com>
2022-11-15 23:50:08 +0900

    2022.11.15 23:50

--- ch06/src/Ex6_4.java
+++ ch06/src/Ex6_4.java
@@ -1,11 +1,17 @@
+import java.util.Iterator;
+
 class Ex6_4 {
 	public static void main(String args[]) {
 		MyMath mm = new MyMath();
-		long result1 = mm.add(5L, 3L);
+		long result1 = mm.add(5L, 3L); // add 메서드 호출 
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
 		double result4 = mm.divide(5L, 3L);
-
+		int result5 = mm.max(5, 3);
+		mm.printGugudan(13);
+		
+		
+		System.out.println("두수 중 큰 값 출력 max(5L, 3L) = " + result5); // 두수중 큰 값을 출려한다. 
 		System.out.println("add(5L, 3L) = " + result1);
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
@@ -14,11 +20,31 @@ class Ex6_4 {
  }
 
  class MyMath {
+	 
+	void printGugudan(int dan) {
+		if (!(2<= dan && dan <= 9)) {
+			System.out.println("구구단 범위를 벗어났습니다." + dan);
+			return ;
+		}
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.printf("%d*d = %d%n",dan,i,dan*i);
+		}
+		
+		// return;
+	}
+	 
 	long add(long a, long b) {
 		long result = a + b;
 		return result;
 	//	return a + b;	// 위의 두 줄을 이와 같이 한 줄로 간단히 할 수 있다.
 	}
+	
+	// 두값을 받아서 큰 값을 반환하는 메서드 
+	int max(int a, int b) {		
+		return a>b ? a:b;
+	}
+	
 	long subtract(long a, long b) { return a - b; }
 	long multiply(long a, long b) { return a * b; }
 	double divide(double a, double b) {
--- ch06/src/Ex6_8.java
+++ ch06/src/Ex6_8.java
@@ -5,12 +5,12 @@ class Ex6_8 {
 		Data3 d = new Data3();
 		d.x = 10;
 
-		Data3 d2 = copy(d); 
+		Data3 d2 = test(d); 
 		System.out.println("d.x ="+d.x);
 		System.out.println("d2.x="+d2.x);
 	}
 
-	static Data3 copy(Data3 d) {
+	static Data3 test(Data3 d) {
 		Data3 tmp = new Data3();    // 새로운 객체 tmp를 생성한다.
 
 		tmp.x = d.x;  // d.x의 값을 tmp.x에 복사한다.
